923ff2463bdcaabbdf8567e0a42624b1abc1d201
atakanapan <atakanapan12@gmail.com>
2023-01-27 23:22:36 +0300

    Devnote to do list app. Building with SwiftUI Masterclass, may make my own version later with updates. Finished core data part mostly.

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Atakan Apan on 1/27/23.
+//
+
+import SwiftUI
+
+//MARK: Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: UI
+//MARK: UX
